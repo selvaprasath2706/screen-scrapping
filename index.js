f5f551f5bdbcc5d8982dc7d5bf635e86caeefcf1
Selva Prasath <sselva@ftdi.com>
2023-04-09 22:11:41 +0530

    initial commit for screen scraping

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const pdfMake = require("pdfmake");
+const fonts = {
+  Roboto: {
+    normal: "fonts/Roboto-Regular.ttf",
+    bold: "fonts/Roboto-Medium.ttf",
+    italics: "fonts/Roboto-Italic.ttf",
+  },
+};
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://dev.to");
+  const data = await page.evaluate(() => {
+    const result = [];
+    const elements = document.querySelectorAll(".crayons-story ");
+    for (let element of elements) {
+      let href = "www.dev.to";
+      const title = element.querySelector(
+        ".crayons-story__hidden-navigation-link"
+      ).innerText;
+      href += element
+        .querySelector(".crayons-story__hidden-navigation-link")
+        .getAttribute("href");
+      result.push({ title, href });
+    }
+    return result;
+  });
+  const docDefinition = {
+    content: [
+      { text: "The data from dev.to is", fontSize: 16 },
+      data.map((indvidualData) => {
+        return [
+          {
+            text: indvidualData.title,
+            link: indvidualData.href,
+            color: "blue",
+          },
+        ];
+      }),
+    ],
+    fonts,
+  };
+
+  const printer = new pdfMake(fonts);
+  const pdfDoc = printer.createPdfKitDocument(docDefinition);
+  pdfDoc.end();
+  const formData = pdfDoc.read();
+  const encodedBase64String = formData.toString("base64");
+  await browser.close();
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
