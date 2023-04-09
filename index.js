f8afb936bde8b12f49448d396d62a2f0c9050801
Selva Prasath <sselva@ftdi.com>
2023-04-09 22:34:46 +0530

    Added a router named get

--- index.js
+++ index.js
@@ -12,49 +12,58 @@ const fonts = {
 const app = express();
 
 app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://dev.to");
-  const data = await page.evaluate(() => {
-    const result = [];
-    const elements = document.querySelectorAll(".crayons-story ");
-    for (let element of elements) {
-      let href = "www.dev.to";
-      const title = element.querySelector(
-        ".crayons-story__hidden-navigation-link"
-      ).innerText;
-      href += element
-        .querySelector(".crayons-story__hidden-navigation-link")
-        .getAttribute("href");
-      result.push({ title, href });
-    }
-    return result;
-  });
-  const docDefinition = {
-    content: [
-      { text: "The data from dev.to is", fontSize: 16 },
-      data.map((indvidualData) => {
-        return [
-          {
-            text: indvidualData.title,
-            link: indvidualData.href,
-            color: "blue",
-          },
-        ];
-      }),
-    ],
-    fonts,
-  };
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://dev.to");
+    const data = await page.evaluate(() => {
+      const result = [];
+      const elements = document.querySelectorAll(".crayons-story ");
+      for (let element of elements) {
+        let href = "www.dev.to";
+        const title = element.querySelector(
+          ".crayons-story__hidden-navigation-link"
+        ).innerText;
+        href += element
+          .querySelector(".crayons-story__hidden-navigation-link")
+          .getAttribute("href");
+        result.push({ title, href });
+      }
+      return result;
+    });
+    const docDefinition = {
+      content: [
+        { text: "The data from dev.to is", fontSize: 16 },
+        data.map((indvidualData) => {
+          return [
+            {
+              text: indvidualData.title,
+              link: indvidualData.href,
+              color: "blue",
+            },
+          ];
+        }),
+      ],
+      fonts,
+    };
 
-  const printer = new pdfMake(fonts);
-  const pdfDoc = printer.createPdfKitDocument(docDefinition);
-  pdfDoc.end();
-  const formData = pdfDoc.read();
-  const encodedBase64String = formData.toString("base64");
-  await browser.close();
-  res.json(data);
+    const printer = new pdfMake(fonts);
+    const pdfDoc = printer.createPdfKitDocument(docDefinition);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", 'attachment; filename="example.pdf"');
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+    const formData = pdfDoc.read();
+    const encodedBase64String = formData.toString("base64");
+    await browser.close();
+    res.json(data);
+  } catch (err) {
+    res.json(err);
+  }
 });
-
+app.get("/get", async (req, res) => {
+  res.send("Reahed get");
+});
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
